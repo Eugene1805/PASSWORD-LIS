92dd8748cd3ae8a510e8a1f809de672a86dd25c1
Eugenio <eugeniosalvadorgonzalez@gmail.com>
2025-09-25 16:26:20 -0600

    ADD test for the conection with SQL Server

--- server/Server/Host/Program.cs
+++ server/Server/Host/Program.cs
@@ -1,15 +1,49 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Host
 {
-    internal class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var sqlBuilder = new SqlConnectionStringBuilder
+                {
+                    DataSource = Environment.GetEnvironmentVariable("DB_SERVER_PASSWORDLIS") ?? "localhost",
+                    InitialCatalog = "PasswordLIS",
+                    UserID = Environment.GetEnvironmentVariable("DB_USER_PASSWORDLIS"),
+                    Password = Environment.GetEnvironmentVariable("DB_PASS_PASSWORDLIS"),
+                    MultipleActiveResultSets = true
+                };
+
+                var entityBuilder = new EntityConnectionStringBuilder
+                {
+                    Provider = "System.Data.SqlClient",
+                    ProviderConnectionString = sqlBuilder.ToString(),
+                    Metadata = "res://*/PasswordLIS.csdl|res://*/PasswordLIS.ssdl|res://*/PasswordLIS.msl"
+                };
+
+                using (var ctx = new PasswordLISEntities(entityBuilder.ToString()))
+                {
+
+                    Console.WriteLine("Conexión establecida con éxito.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al establecer la conexión: " + ex.Message);
+                
+            }
+
         }
     }
 }
