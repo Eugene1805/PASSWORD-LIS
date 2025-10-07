a43a6ee082a52b232301f161b6674f2e0329e543
Eugenio <eugeniosalvadorgonzalez@gmail.com>
2025-10-07 15:41:47 -0600

    ADD Util for conection string

--- server/Server/Data/DAL/AcountRepository.cs
+++ server/Server/Data/DAL/AcountRepository.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Data.Model;
+using Data.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data.DAL
 {
-    internal class AcountRepository
+    public class AcountRepository
     {
+        public bool CreateAccount(UserAccount acount)
+        {
+            using (var context = new PasswordLISEntities(Connection.GetConnectionString()))
+            {
+                try
+                {
+                    context.UserAccount.Add(acount);
+                    context.SaveChanges();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+           
+        }
     }
 }
---
+++ server/Server/Data/Util/Connection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Util
+{
+    public static class Connection
+    {
+        public static String GetConnectionString()
+        {
+            var sqlBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = Environment.GetEnvironmentVariable("DB_SERVER_PASSWORDLIS") ?? "localhost",
+                InitialCatalog = "PasswordLIS",
+                UserID = Environment.GetEnvironmentVariable("DB_USER_PASSWORDLIS"),
+                Password = Environment.GetEnvironmentVariable("DB_PASS_PASSWORDLIS"),
+                MultipleActiveResultSets = true
+            };
+
+            var entityBuilder = new EntityConnectionStringBuilder
+            {
+                Provider = "System.Data.SqlClient",
+                ProviderConnectionString = sqlBuilder.ToString(),
+                Metadata = "res://*/PasswordLIS.csdl|res://*/PasswordLIS.ssdl|res://*/PasswordLIS.msl"
+            };
+            return entityBuilder.ToString();
+        }
+    }
+}
