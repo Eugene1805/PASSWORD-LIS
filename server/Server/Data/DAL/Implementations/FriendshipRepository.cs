45950a6197df8b9f8bb910acdf4abd15f875379e
Eugenio <eugeniosalvadorgonzalez@gmail.com>
2025-10-18 17:10:53 -0600

    REMOVE unnecessary usings

--- server/Server/Data/DAL/Implementations/FriendshipRepository.cs
+++ server/Server/Data/DAL/Implementations/FriendshipRepository.cs
@@ -5,8 +5,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Data.Entity;
-using System.Text;
-using System.Threading.Tasks;
 using Data.Util;
 
 namespace Data.DAL.Implementations
--- server/Server/Data/Util/Connection.cs
+++ server/Server/Data/Util/Connection.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.Entity.Core.EntityClient;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Data.Util
 {
