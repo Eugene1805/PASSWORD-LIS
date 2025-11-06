95aa5c3f2a7efa3cab5d635c52f0e67db583c944
Eugenio <eugeniosalvadorgonzalez@gmail.com>
2025-11-06 07:34:05 -0600

    ADD round validations

---
+++ server/Server/Services/Contracts/DTOs/ValidationResultDTO.cs
@@ -0,0 +1,24 @@
+﻿using Services.Contracts.Enums;
+using System.Runtime.Serialization;
+
+namespace Services.Contracts.DTOs
+{
+    [DataContract]
+    public class ValidationResultDTO
+    {
+        [DataMember]
+        public MatchTeam TeamThatWasValidated { get; set; }
+
+        [DataMember]
+        public int TotalPenaltyApplied { get; set; }
+
+        [DataMember]
+        public int NewRedTeamScore { get; set; }
+
+        [DataMember]
+        public int NewBlueTeamScore { get; set; }
+
+        [DataMember]
+        public string Message { get; set; } // Ej: "Penalización de -3 puntos al Equipo Rojo"
+    }
+}
