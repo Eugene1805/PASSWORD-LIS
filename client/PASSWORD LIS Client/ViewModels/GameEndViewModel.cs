fea73e78b4c7b6574f64f839283295b2f39cd0e6
Arturo <artcupe@gmail.com>
2025-11-14 00:23:41 -0600

    Eq Ganador y perdedor, salta communicationException

---
+++ client/PASSWORD LIS Client/ViewModels/GameEndViewModel.cs
@@ -0,0 +1,41 @@
+﻿using PASSWORD_LIS_Client.Commands;
+using PASSWORD_LIS_Client.Utils;
+using PASSWORD_LIS_Client.Views;
+using System.Windows.Input;
+
+namespace PASSWORD_LIS_Client.ViewModels
+{
+    public class GameEndViewModel : BaseViewModel
+    {
+        private string redTeamScore;
+        public string RedTeamScore
+        {
+            get => redTeamScore;
+            set => SetProperty(ref redTeamScore, value);
+        }
+        private string blueTeamScore;
+        public string BlueTeamScore
+        {
+            get => blueTeamScore;
+            set => SetProperty(ref blueTeamScore, value);
+        }
+
+        public ICommand BackToLobbyCommand { get; }
+
+        private readonly IWindowService windowService;
+
+        public GameEndViewModel(int redScore, int blueScore, IWindowService windowService)
+        {
+            this.windowService = windowService;
+            RedTeamScore = string.Format(Properties.Langs.Lang.redTeamText + ": {0}", redScore);
+            BlueTeamScore = string.Format(Properties.Langs.Lang.blueTeamText + ": {0}", blueScore);
+
+            BackToLobbyCommand = new RelayCommand(BackToLobby);
+        }
+
+        private void BackToLobby(object obj)
+        {
+            windowService.NavigateTo(new LobbyPage());
+        }
+    }
+}
