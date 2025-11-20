52e98b00ffb98e4404fc7d34b7e906e3a8dad311
Eugenio <eugeniosalvadorgonzalez@gmail.com>
2025-11-19 19:13:25 -0600

    ADD exception handling for the avatarhelper

--- client/PASSWORD LIS Client/Utils/AvatarHelper.cs
+++ client/PASSWORD LIS Client/Utils/AvatarHelper.cs
@@ -1,18 +1,33 @@
 ﻿using System;
+using System.Configuration;
 
 namespace PASSWORD_LIS_Client.Utils
 {
     public static class AvatarHelper
     {
+        #pragma warning disable S1075
+        private const string DefaultPackSchema = "pack://application:,,,/";
+        #pragma warning restore S1075
         public static Uri GetAvatarUriById(int photoId)
         {
             int normalizedId = (photoId >= 1 && photoId <= 6) ? photoId : 0;
             string fileName = normalizedId == 0 ? "AvatarDefault.png" : $"Avatar{normalizedId}.png";
 
             string relativePath = $"Resources/{fileName}";
-
-            var baseUri = new Uri("pack://application:,,,/", UriKind.Absolute);
-            return new Uri(baseUri, relativePath); // Absolute pack URI result
+            string schema = ConfigurationManager.AppSettings["WPFSchema"];
+            if (string.IsNullOrEmpty(schema) || !Uri.IsWellFormedUriString(schema, UriKind.Absolute))
+            {
+                schema = DefaultPackSchema;
+            }
+            try
+            {
+                var baseUri = new Uri(schema, UriKind.Absolute);
+                return new Uri(baseUri, relativePath);
+            }
+            catch (UriFormatException)
+            {
+                return new Uri(new Uri(DefaultPackSchema), relativePath);
+            }
         }
     }
 }
