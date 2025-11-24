1c30ede3126b69dd3a0ecd286c45ced601f4edcd
Eugenio <eugeniosalvadorgonzalez@gmail.com>
2025-11-24 10:37:40 -0600

    UPDATE ProfileManager to use ServiceBase

--- server/Server/Services/Services/ProfileManager.cs
+++ server/Server/Services/Services/ProfileManager.cs
@@ -3,12 +3,7 @@ using Data.Model;
 using log4net;
 using Services.Contracts;
 using Services.Contracts.DTOs;
-using Services.Contracts.Enums;
-using Services.Util;
-using System;
 using System.Collections.Generic;
-using System.Data.Common;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.ServiceModel;
 using System.Threading.Tasks;
@@ -16,27 +11,26 @@ using System.Threading.Tasks;
 namespace Services.Services
 {
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
-    public class ProfileManager : IProfileManager
+    public class ProfileManager : ServiceBase, IProfileManager
     {
         private readonly IAccountRepository repository;
         private static readonly ILog log = LogManager.GetLogger(typeof(ProfileManager));
 
-        public ProfileManager(IAccountRepository accountRepository)
+        public ProfileManager(IAccountRepository accountRepository) :base(log)
         {
             repository = accountRepository;
         }
 
         public async Task<UserDTO> UpdateProfileAsync(UserDTO updatedProfileData)
         {
-            if (updatedProfileData == null || updatedProfileData.PlayerId <= 0)
+            return await ExecuteAsync(async () =>
             {
-                log.WarnFormat("UpdateProfile called with null data or invalid PlayerId {0}",
-                    updatedProfileData?.PlayerId);
-                return null;
-            }
-
-            try
-            {
+                if (updatedProfileData == null || updatedProfileData.PlayerId <= 0)
+                {
+                    log.WarnFormat("UpdateProfile called with null data or invalid PlayerId {0}",
+                        updatedProfileData?.PlayerId);
+                    return null;
+                }
                 log.InfoFormat("Attempting to update profile for PlayerId: {0}", updatedProfileData.PlayerId);
 
                 var accountData = MapToUserAccount(updatedProfileData);
@@ -59,28 +53,9 @@ namespace Services.Services
                         updatedProfileData.PlayerId);
                     return null;
                 }
-            }
-            catch (DbUpdateException dbUpEx)
-            {
-                log.Error($"DbUpdateException updating profile PlayerId: {updatedProfileData.PlayerId}." +
-                    $" Ex: {dbUpEx.Message}", dbUpEx);
-                throw FaultExceptionFactory.Create(ServiceErrorCode.DatabaseError, "DATABASE_ERROR",
-                    "An error occurred while trying to save the changes to the database.");
-            }
-            catch (DbException dbEx)
-            {
-                log.Error($"DbException updating profile PlayerId: {updatedProfileData.PlayerId}. " +
-                    $"Ex: {dbEx.Message}", dbEx);
-                throw FaultExceptionFactory.Create( ServiceErrorCode.DatabaseError, "DATABASE_ERROR", 
-                    "A database communication error occurred while updating the profile.");
-            }
-            catch (Exception ex)
-            {
-                log.Fatal($"Unexpected error in UpdateProfile for PlayerId: {updatedProfileData.PlayerId}. " +
-                    $"Ex: {ex.Message}", ex);
-                throw FaultExceptionFactory.Create(ServiceErrorCode.UnexpectedError, "UNEXPECTED_ERROR", 
-                    "An unexpected server error occurred while updating the profile.");
-            }
+
+            }, context:"ProfileManager: UpdateProfileAsync");
+            
         }
 
         private UserAccount MapToUserAccount(UserDTO dto)
