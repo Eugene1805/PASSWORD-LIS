1d4b80e7bec3b1c8149dc8ffb3057d90023f7c6d
Arturo <artcupe@gmail.com>
2025-12-10 21:00:19 -0600

    Validaciones faltantes Login

--- client/PASSWORD LIS Client/ViewModels/LoginViewModel.cs
+++ client/PASSWORD LIS Client/ViewModels/LoginViewModel.cs
@@ -5,7 +5,6 @@ using PASSWORD_LIS_Client.Utils;
 using PASSWORD_LIS_Client.Views;
 using System;
 using System.Collections.Generic;
-using System.ServiceModel;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -13,18 +12,38 @@ namespace PASSWORD_LIS_Client.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
+        private readonly ILoginManagerService loginManagerService;
+
+        private const int MaximumEmailLength = 100;
+        private const int MaximumPasswordLength = 15;
+        private const int MinimumGuestRandomNumber = 1000;
+        private const int MaximumGuestRandomNumber = 9999;
+        private const int GuestPlayerId = -1;
+        private const int DefaultPhotoId = 0;
+        private const int MinimumValidUserId = 0;
+
         private string email;
         public string Email
         {
             get => email;
-            set { email = value; OnPropertyChanged(); }
+            set 
+            { 
+                email = value;
+                ValidateEmail();
+                OnPropertyChanged(); 
+            }
         }
 
         private string password;
         public string Password
         {
             get => password;
-            set { password = value; OnPropertyChanged(); }
+            set 
+            { 
+                password = value;
+                ValidatePassword();
+                OnPropertyChanged(); 
+            }
         }
 
         private bool isLoggingIn;
@@ -34,12 +53,24 @@ namespace PASSWORD_LIS_Client.ViewModels
             set { isLoggingIn = value; OnPropertyChanged(); }
         }
 
+        private string emailError;
+        public string EmailError
+        {
+            get => emailError;
+            set { emailError = value; OnPropertyChanged(); }
+        }
+
+        private string passwordError;
+        public string PasswordError
+        {
+            get => passwordError;
+            set { passwordError = value; OnPropertyChanged(); }
+        }
+
         public ICommand LoginCommand { get; }
         public ICommand PlayAsGuestCommand { get; }
         public ICommand NavigateToSignUpCommand { get; }
         public ICommand NavigateToForgotPasswordCommand { get; }
-
-        private readonly ILoginManagerService loginManagerService;
 
         public LoginViewModel(ILoginManagerService loginManagerService, IWindowService windowService) 
             : base(windowService)
@@ -54,39 +85,20 @@ namespace PASSWORD_LIS_Client.ViewModels
 
         private bool CanLogin()
         {
-            return !string.IsNullOrWhiteSpace(Email) && !string.IsNullOrWhiteSpace(Password) && !IsLoggingIn;
+            return !IsLoggingIn;
         }
 
         private async Task LoginAsync()
         {
+            if (!AreFieldsValid())
+            {
+                return;
+            }
+
             IsLoggingIn = true;
             try
             {
-                await ExecuteAsync(async () =>
-                {
-                    var loggedInUser = await loginManagerService.LoginAsync(Email, Password);
-                    bool isAccountVerified = false;
-
-                    if (loggedInUser.UserAccountId > 0) 
-                    {
-                        isAccountVerified = await loginManagerService.IsAccountVerifiedAsync(Email);
-                    }
-
-                    if (loggedInUser.UserAccountId > 0 && isAccountVerified)
-                    {
-                        ProcessSuccessfulLogin(loggedInUser);
-                    }
-                    else if (loggedInUser.UserAccountId > 0 && !isAccountVerified)
-                    {
-                        await loginManagerService.SendVerificationCodeAsync(loggedInUser.Email);
-                        VerifyAccount(loggedInUser.Email);
-                    }
-                    else
-                    {
-                        windowService.ShowPopUp(Properties.Langs.Lang.warningTitleText,
-                            Properties.Langs.Lang.wrongCredentialsText, PopUpIcon.Warning);
-                    }
-                });
+                await ExecuteAsync(ProcessLoginSequenceAsync);
             }
             finally
             {
@@ -94,6 +106,31 @@ namespace PASSWORD_LIS_Client.ViewModels
             }
         }
 
+        private async Task ProcessLoginSequenceAsync()
+        {
+            var loggedInUser = await loginManagerService.LoginAsync(Email, Password);
+            bool isAccountVerified = false;
+
+            if (loggedInUser.UserAccountId > MinimumValidUserId)
+            {
+                isAccountVerified = await loginManagerService.IsAccountVerifiedAsync(Email);
+            }
+
+            if (loggedInUser.UserAccountId > MinimumValidUserId && isAccountVerified)
+            {
+                ProcessSuccessfulLogin(loggedInUser);
+            }
+            else if (loggedInUser.UserAccountId > MinimumValidUserId && !isAccountVerified)
+            {
+                await loginManagerService.SendVerificationCodeAsync(loggedInUser.Email);
+                VerifyAccount(loggedInUser.Email);
+            }
+            else
+            {
+                windowService.ShowPopUp(Properties.Langs.Lang.warningTitleText,
+                    Properties.Langs.Lang.wrongCredentialsText, PopUpIcon.Warning);
+            }
+        }
         private void ProcessSuccessfulLogin(UserDTO loggedInUser)
         {
             SessionManager.Login(loggedInUser);
@@ -107,14 +144,14 @@ namespace PASSWORD_LIS_Client.ViewModels
 
         private void PlayAsGuest(object parameter)
         {
-            string guestNickname = Properties.Langs.Lang.guestText + new Random().Next(1000, 9999);
+            string guestNickname = Properties.Langs.Lang.guestText + new Random().Next(MinimumGuestRandomNumber, MaximumGuestRandomNumber);
             var guestUser = new UserDTO
             {
-                PlayerId = -1,
+                PlayerId = GuestPlayerId,
                 Nickname = guestNickname,
                 FirstName = Properties.Langs.Lang.guestText,
-                LastName = "",
-                PhotoId = 0,
+                LastName = string.Empty,
+                PhotoId = DefaultPhotoId,
                 SocialAccounts = new Dictionary<string, string>()
             };
 
@@ -140,8 +177,8 @@ namespace PASSWORD_LIS_Client.ViewModels
 
         private void NavigateToForgotPassword(object parameter)
         {
-            var retreivePasswordViewModel = new RetrievePasswordViewModel(App.PasswordResetManagerService, windowService);
-            var retrievePasswordWindow = new RetrievePasswordWindow { DataContext = retreivePasswordViewModel};
+            var retrivePasswordViewModel = new RetrievePasswordViewModel(App.PasswordResetManagerService, windowService);
+            var retrievePasswordWindow = new RetrievePasswordWindow { DataContext = retrivePasswordViewModel};
             retrievePasswordWindow.ShowDialog();
             windowService.CloseWindow(this);
         }
@@ -151,6 +188,50 @@ namespace PASSWORD_LIS_Client.ViewModels
             windowService.ShowVerifyCodeWindow(email, VerificationReason.AccountActivation);
             windowService.CloseWindow(this);
             windowService.CloseMainWindow();
+        }
+
+        private bool AreFieldsValid()
+        {
+            bool isEmailValid = ValidateEmail();
+            bool isPasswordValid = ValidatePassword();
+
+            return isEmailValid && isPasswordValid;
+        }
+
+        private bool ValidateEmail()
+        {
+            if (string.IsNullOrWhiteSpace(Email))
+            {
+                EmailError = Properties.Langs.Lang.emptyEmailText;
+                return false;
+            }
+
+            if (Email.Length > MaximumEmailLength)
+            {
+                EmailError = Properties.Langs.Lang.emailTooLongText;
+                return false;
+            }
+
+            EmailError = null;
+            return true;
+        }
+
+        private bool ValidatePassword()
+        {
+            if (string.IsNullOrWhiteSpace(Password))
+            {
+                PasswordError = Properties.Langs.Lang.emptyPasswordText;
+                return false;
+            }
+
+            if (Password.Length > MaximumPasswordLength)
+            {
+                PasswordError = Properties.Langs.Lang.passwordTooLongText;
+                return false;
+            }
+
+            PasswordError = null;
+            return true;
         }
     }
 }
