87f53c68f39f17f7900ab3bc740275351a04f6ea
Eugenio <eugeniosalvadorgonzalez@gmail.com>
2025-12-11 18:32:01 -0600

    Add  {}

--- server/Server/Test/ServicesTests/ReportManagerTests.cs
+++ server/Server/Test/ServicesTests/ReportManagerTests.cs
@@ -47,17 +47,26 @@ namespace Test.ServicesTests
         {
             await Assert.ThrowsAsync<FaultException<ServiceErrorDetailDTO>>(() => sut.SubmitReportAsync(null));
 
-            var bad = new ReportDTO { ReporterPlayerId = 0, ReportedPlayerId = 2 };
+            var bad = new ReportDTO 
+            { 
+                ReporterPlayerId = 0, ReportedPlayerId = 2 
+            };
             await Assert.ThrowsAsync<FaultException<ServiceErrorDetailDTO>>(() => sut.SubmitReportAsync(bad));
 
-            var self = new ReportDTO { ReporterPlayerId = 5, ReportedPlayerId = 5 };
+            var self = new ReportDTO 
+            { 
+                ReporterPlayerId = 5, ReportedPlayerId = 5 
+            };
             await Assert.ThrowsAsync<FaultException<ServiceErrorDetailDTO>>(() => sut.SubmitReportAsync(self));
         }
 
         [Fact]
         public async Task SubmitReportAsync_ShouldThrow_WhenReporterNotFound()
         {
-            var dto = new ReportDTO { ReporterPlayerId = 1, ReportedPlayerId = 2, Reason = "x" };
+            var dto = new ReportDTO 
+            { 
+                ReporterPlayerId = 1, ReportedPlayerId = 2, Reason = "x" 
+            };
             playerRepository.Setup(r => r.GetPlayerByIdAsync(1)).ReturnsAsync((Player?)null);
             playerRepository.Setup(r => r.GetPlayerByIdAsync(2)).ReturnsAsync(MakePlayer(2, "Reported"));
 
@@ -68,7 +77,10 @@ namespace Test.ServicesTests
         [Fact]
         public async Task SubmitReportAsync_ShouldThrow_WhenReportedNotFound()
         {
-            var dto = new ReportDTO { ReporterPlayerId = 1, ReportedPlayerId = 2, Reason = "x" };
+            var dto = new ReportDTO 
+            { 
+                ReporterPlayerId = 1, ReportedPlayerId = 2, Reason = "x" 
+            };
             playerRepository.Setup(r => r.GetPlayerByIdAsync(1)).ReturnsAsync(MakePlayer(1, "Reporter"));
             playerRepository.Setup(r => r.GetPlayerByIdAsync(2)).ReturnsAsync((Player?)null);
 
@@ -79,7 +91,10 @@ namespace Test.ServicesTests
         [Fact]
         public async Task SubmitReportAsync_ShouldThrow_WhenDuplicateSinceLastBan()
         {
-            var dto = new ReportDTO { ReporterPlayerId = 1, ReportedPlayerId = 2, Reason = "dup" };
+            var dto = new ReportDTO 
+            { 
+                ReporterPlayerId = 1, ReportedPlayerId = 2, Reason = "dup" 
+            };
             playerRepository.Setup(r => r.GetPlayerByIdAsync(1)).ReturnsAsync(MakePlayer(1, "Reporter"));
             playerRepository.Setup(r => r.GetPlayerByIdAsync(2)).ReturnsAsync(MakePlayer(2, "Reported"));
             banRepository.Setup(b => b.GetLastBanEndTimeAsync(2)).ReturnsAsync((DateTime?)null);
@@ -92,7 +107,10 @@ namespace Test.ServicesTests
         [Fact]
         public async Task SubmitReportAsync_ShouldAddReport_AndNotifyCallback_WhenValid()
         {
-            var dto = new ReportDTO { ReporterPlayerId = 1, ReportedPlayerId = 2, Reason = "ok" };
+            var dto = new ReportDTO 
+            { 
+                ReporterPlayerId = 1, ReportedPlayerId = 2, Reason = "ok" 
+            };
             playerRepository.Setup(r => r.GetPlayerByIdAsync(1)).ReturnsAsync(MakePlayer(1, "RepNick"));
             playerRepository.Setup(r => r.GetPlayerByIdAsync(2)).ReturnsAsync(MakePlayer(2, "TargetNick"));
             banRepository.Setup(b => b.GetLastBanEndTimeAsync(2)).ReturnsAsync((DateTime?)null);
@@ -113,7 +131,10 @@ namespace Test.ServicesTests
         [Fact]
         public async Task SubmitReportAsync_ShouldBanPlayer_WhenThresholdReached()
         {
-            var dto = new ReportDTO { ReporterPlayerId = 3, ReportedPlayerId = 4, Reason = "ban" };
+            var dto = new ReportDTO 
+            { 
+                ReporterPlayerId = 3, ReportedPlayerId = 4, Reason = "ban" 
+            };
             playerRepository.Setup(r => r.GetPlayerByIdAsync(3)).ReturnsAsync(MakePlayer(3, "R"));
             playerRepository.Setup(r => r.GetPlayerByIdAsync(4)).ReturnsAsync(MakePlayer(4, "T"));
             banRepository.Setup(b => b.GetLastBanEndTimeAsync(4)).ReturnsAsync((DateTime?)null);
@@ -147,7 +168,10 @@ namespace Test.ServicesTests
         public async Task IsPlayerBannedAsync_ShouldReturnTrue_WhenActiveBanExists()
         {
             banRepository.Setup(b => b.GetActiveBanForPlayerAsync(15)).ReturnsAsync(
-                new Ban { PlayerId = 15, EndTime = DateTime.UtcNow.AddMinutes(10) });
+                new Ban 
+                { 
+                    PlayerId = 15, EndTime = DateTime.UtcNow.AddMinutes(10) 
+                });
             var res = await sut.IsPlayerBannedAsync(15);
             Assert.True(res);
         }
